34777d9f7a86d44451d404da07f9cfced9c962f5
Rahul Patel <rr710505@gmail.com>
2024-02-26 21:22:09 +0530

    add hooks folder update usestate file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Button from './components/button/Button';
+// import Button from './components/button/Button'; 
+import Usestate from './hooks/Usestate';
 // import Circle from './components/circle/Circle';
 // import Navbar from "./components/navbar/Navbar"
 
@@ -9,9 +10,23 @@ function App() {
        {/* <Card/> */}
        {/* <Navbar/> */}
        {/* <Circle/> */}
-       <Button/>
+       {/* <Button/> */}
+       <Usestate/>
     </div>
   );
 }
 
 export default App;
+
+
+// find second largest elements in the array
+
+// const arr = [12,4,3,2,8,9,54,16,16,16]
+
+// const uniq = [...new Set(arr)]
+
+// const result = uniq.sort((a,b)=>{
+//  return b-a
+// })
+
+// console.log(result[1])
